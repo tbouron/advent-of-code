030d10620af32130d1e43b8cc92ecb6c11e38258
Thomas Bouron <thomas.bouron@cloudsoftcorp.com>
2021-12-01 23:05:55 +0000

    Add day 1 extra challenge

--- src/day-1.js
+++ src/day-1.js
@@ -1,14 +1,25 @@
 import * as fs from 'fs';
 
 const inputFile = '../input/day-1.txt';
-const increaseCount = fs.readFileSync(inputFile, 'utf8')
+const parsedInput = fs.readFileSync(inputFile, 'utf8')
     .split('\n')
-    .map(item => parseInt(item))
+    .map(item => parseInt(item));
+
+const calculateIncrease = list => list.reduce((acc, item, index, list) => {
+    if (index > 0) {
+        acc += item > list[index - 1] ? 1 : 0;
+    }
+    return acc;
+}, 0);
+
+const increaseCount = calculateIncrease(parsedInput);
+const slidingWindowsCount = calculateIncrease(parsedInput
     .reduce((acc, item, index, list) => {
-        if (index > 0) {
-            acc += item > list[index -1] ? 1 : 0;
+        if (index > 2) {
+            acc.push(item + list[index - 1] + list[index - 2]);
         }
         return acc;
-    }, 0);
+    }, []));
 
 console.log(`Based on the input file "${inputFile}", the depth increases "${increaseCount}" times`);
+console.log(`Based on the input file "${inputFile}", the sliding windows increase "${slidingWindowsCount}" times`);
