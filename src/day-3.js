9e47b2797ee98300f23ecc33a21890600a3afcad
Thomas Bouron <thomas.bouron@cloudsoftcorp.com>
2021-12-03 15:15:04 +0000

    Day 3: Add part #2

--- src/day-3.js
+++ src/day-3.js
@@ -1,9 +1,26 @@
 const fs = require('fs');
 const path = require('path');
 
-const parsedInput = fs.readFileSync(path.join(__dirname, `${path.basename(__filename, '.js')}.txt`), 'utf8')
+const mostCommonBit = bits => {
+    let maxCount = 0;
+
+    return bits.reduce((acc, bit) => {
+        acc[bit]++;
+        return acc;
+    }, [0, 0]).reduce((acc, count, bit) => {
+        if (count >= maxCount) {
+            maxCount = count;
+            return bit;
+        }
+        return acc;
+    }, 0);
+};
+
+const baseInput = fs.readFileSync(path.join(__dirname, `${path.basename(__filename, '.js')}.txt`), 'utf8')
     .split('\n')
-    .filter(line => line.trim() !== '')
+    .filter(line => line.trim() !== '');
+
+const gammaRate = baseInput
     .reduce((acc, line) => {
         line.split('').forEach((bit, index) => {
             if (!Array.isArray(acc[index])) {
@@ -12,24 +29,34 @@ const parsedInput = fs.readFileSync(path.join(__dirname, `${path.basename(__file
             acc[index].push(parseInt(bit));
         })
         return acc;
-    }, []);
-
-const gammaRate = parsedInput.map(bits => {
-    let maxCount = 0;
-
-    return bits.reduce((acc, bit) => {
-        acc[bit]++;
-        return acc;
-    }, [0, 0]).reduce((acc, count, bit) => {
-        if (count > maxCount) {
-            maxCount = count;
-            return bit;
-        }
-        return acc;
-    }, 0);
-});
+    }, [])
+    .map(mostCommonBit);
 const epsilonRate = gammaRate.map(bit => bit ^ 1);
 
+const findRatingFor = (inputLines, flip = false, search = '', bitCriteriaIndex = 0) => {
+    let bitCriteriaAtIndex = mostCommonBit(inputLines
+        .map(inputLine => parseInt(inputLine.substr(bitCriteriaIndex, 1))));
+    if (flip) {
+        bitCriteriaAtIndex = bitCriteriaAtIndex ^ 1;
+    }
+
+    search += bitCriteriaAtIndex;
+
+    const filteredLines = inputLines.filter(inputLine => inputLine.startsWith(search));
+
+    if (filteredLines.length > 1) {
+        return findRatingFor(filteredLines, flip, search, bitCriteriaIndex + 1);
+    } else if (filteredLines.length === 1) {
+        return filteredLines[0]
+    } else {
+        throw new Error('Hum, that is not supposed to happen!');
+    }
+}
+
+const oxygenRate = findRatingFor(baseInput);
+const co2ScrubberRate = findRatingFor(baseInput, true);
+
 module.exports = {
-    'Part #1': parseInt(gammaRate.join(''), 2) * parseInt(epsilonRate.join(''), 2)
+    'Part #1': parseInt(gammaRate.join(''), 2) * parseInt(epsilonRate.join(''), 2),
+    'Part #2': parseInt(oxygenRate, 2) * parseInt(co2ScrubberRate, 2)
 };
