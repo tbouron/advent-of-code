c1fefc859c0d0f721593a42d18f5eac24aa65b39
Thomas Bouron <thomas.bouron@cloudsoftcorp.com>
2021-12-04 11:49:29 +0000

    Improve error handling for the main runner

--- index.js
+++ index.js
@@ -11,12 +11,32 @@ const filteredDayFiles = fs.readdirSync(sourceFolder)
 if (filteredDayFiles.length === 0) {
     console.log(`Nothing to run`);
 } else {
-    Promise.all(filteredDayFiles.map(dayFile => import(`./${path.join(sourceFolder, dayFile)}`)))
-        .then(dayExports => {
-            dayExports.forEach((dayExport, index) => {
-                console.log(Array.of(`==== DAY #${dayExports.length === 1 ? dayToRun : index + 1}`)
-                    .concat(Object.entries(dayExport.default).map(([exportKey, exportValue]) => `${exportKey} => ${exportValue}`))
-                    .join('\n'));
-            });
-        }).catch(ex => console.error(ex));
+    Promise.allSettled(filteredDayFiles.map(dayFile => {
+        const match = dayFile.match(/day-(.*).js/);
+        const dayNumber = match !== null ? match[1] : '???';
+        return import(`./${path.join(sourceFolder, dayFile)}`).then(dayExports => {
+            return {
+                day: dayNumber,
+                exports: dayExports
+            };
+        }).catch(error => {
+            throw {
+                day: dayNumber,
+                error: error
+            };
+        })
+    })).then(dayImports => {
+        dayImports.forEach((dayImport) => {
+            const log = [];
+            switch (dayImport.status) {
+                case 'fulfilled':
+                    log.push(`==== DAY #${dayImport.value.day}`, ...Object.entries(dayImport.value.exports.default).map(([exportKey, exportValue]) => `${exportKey} => ${exportValue}`));
+                    break
+                case 'rejected':
+                    log.push(`==== DAY #${dayImport.reason.day}`, `[x] ${dayImport.reason.error}`);
+                    break;
+            }
+            console.log(log.join('\n'));
+        });
+    }).catch(ex => console.error(ex));
 }
