fb1524ee0093dba2484d7bfa9a5107b370ee3535
Thomas Bouron <thomas.bouron@cloudsoftcorp.com>
2021-12-05 13:43:06 +0000

    Day 5: Add part #2

--- src/day-5.js
+++ src/day-5.js
@@ -6,6 +6,12 @@ class Point {
     y = 0;
 
     constructor(x, y) {
+        if (typeof x !== 'number') {
+            throw new Error(`X coordinate must be a number. Gave: ${x} (${typeof x})`);
+        }
+        if (typeof y !== 'number') {
+            throw new Error(`Y coordinate must be a number. Gave: ${y} (${typeof y})`);
+        }
         if (x < 0) {
             throw new Error('X coordinate must be positive');
         }
@@ -25,9 +31,6 @@ class Line {
         if (!start || !end) {
             throw new Error('start and end points must not be null');
         }
-        if (start.x !== end.x && start.y !== end.y) {
-            throw new Error(`The given points do not form an horizontal or vertical line`);
-        }
         this.start = start;
         this.end = end;
     }
@@ -39,6 +42,11 @@ class Line {
     isHorizontal() {
         return this.start.y === this.end.y;
     }
+
+    // A line only ever horizontal, vertical or diagonal.
+    isDiagonal() {
+        return !this.isHorizontal() && !this.isVertical()
+    }
 }
 
 class Grid {
@@ -46,77 +54,59 @@ class Grid {
     columns = 0;
     lines = [];
 
-    constructor(rows, columns) {
-        this.rows = rows;
-        this.columns = columns;
+    constructor(lines) {
+        this.lines = lines;
+        this.rows = Math.max(...lines.flatMap(line => [line.start.y, line.end.y]));
+        this.columns = Math.max(...lines.flatMap(line => [line.start.x, line.end.x]));
     }
 
-    addLine(line) {
-        if (line.start.x > this.columns || line.start.y > this.rows || line.end.x > this.columns || line.end.y > this.rows) {
-            throw new Error(`The line ${line} does not fit into the grid bounds`);
-        }
-        this.lines.push(line);
-    }
-
-    getOverlapPoints() {
+    countOverlappingPoints() {
         const grid = Array(this.rows + 1)
             .fill([])
             .map(() => Array(this.columns + 1)
                 .fill(0)
                 .map(() => 0));
-        this.lines.map(line => {
-            if (line.isHorizontal()) {
-                // console.log(`Horizontal`, line);
-                for (let x = Math.min(line.start.x, line.end.x); x <= Math.max(line.start.x, line.end.x); x++) {
-                    // console.log(`Point: ${x},${line.start.y} – current value: ${grid[line.start.y][x]}`);
 
-                    grid[line.start.y][x] = grid[line.start.y][x] + 1;
-                    // console.log(grid);
+        this.lines.map(line => {
+            if (line.isDiagonal()) {
+                const directionX = line.end.x > line.start.x ? 1 : -1;
+                const directionY = line.end.y > line.start.y ? 1 : -1;
+                for (let i = 0; i <= Math.abs(line.end.x - line.start.x); i++) {
+                    grid[line.start.y + i * directionY][line.start.x + i * directionX]++;
                 }
-            }
-            if (line.isVertical()) {
-                // console.log(`Vertical`, line);
-                // console.log(startX, endX);
-                for (let y = Math.min(line.start.y, line.end.y); y <= Math.max(line.start.y, line.end.y); y++) {
-                    // console.log(`Point covered => ${x},${line.start.y}`);
-                    grid[y][line.start.x] = grid[y][line.start.x] + 1;
+            } else {
+                const startX = Math.min(line.start.x, line.end.x);
+                const endX = Math.max(line.start.x, line.end.x);
+                const startY = Math.min(line.start.y, line.end.y);
+                const endY = Math.max(line.start.y, line.end.y);
+                for (let y = startY; y <= endY; y++) {
+                    for (let x = startX; x <= endX; x++) {
+                        grid[y][x]++;
+                    }
                 }
             }
         });
 
-        console.log(grid);
-
-        return grid.reduce((count, column) => {
-            return count + column.filter(item => item >= 2).length;
+        return grid.reduce((count, row) => {
+            return count + row.filter(item => item >= 2).length;
         }, 0);
     }
 }
 
-let maxRows = 0;
-let maxColumns = 0;
-const grid = fs.readFileSync(path.join(__dirname, `${path.basename(__filename, '.js')}.txt`), 'utf8')
+const lines = fs.readFileSync(path.join(__dirname, `${path.basename(__filename, '.js')}.txt`), 'utf8')
     .split('\n')
     .map(l => {
         let matches = l.trim().match(/([0-9]+),([0-9]+)\s+\->\s+([0-9]+),([0-9]+)/);
         if (matches === null) {
             return null;
         }
-        const start = new Point(matches[1], matches[2]);
-        const end = new Point(matches[3], matches[4]);
-        maxRows = Math.max(matches[2], matches[4], maxRows);
-        maxColumns = Math.max(matches[1], matches[3], maxColumns);
-        try {
-            return new Line(start, end);
-        } catch (e) {
-            return null;
-        }
-    })
-    .filter(line => line !== null)
-    .reduce((grid, line) => {
-        grid.addLine(line);
-        return grid;
-    }, new Grid(maxRows, maxColumns));
+        const start = new Point(parseInt(matches[1]), parseInt(matches[2]));
+        const end = new Point(parseInt(matches[3]), parseInt(matches[4]));
+        return new Line(start, end);
+    });
+const hAndVLines = lines.filter(line => line.isHorizontal() || line.isVertical());
 
 module.exports = {
-    'Part #1': grid.getOverlapPoints()
+    'Part #1': new Grid(hAndVLines).countOverlappingPoints(),
+    'Part #2': new Grid(lines).countOverlappingPoints()
 }
