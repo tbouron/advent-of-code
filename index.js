26e6ebef17836973a4477977023059c9409391ab
Thomas Bouron <thomas.bouron@cloudsoftcorp.com>
2021-12-07 15:21:41 +0000

    Revamp again the runner script to simplify it and add execution time + memory consumption for each day

--- index.js
+++ index.js
@@ -1,42 +1,50 @@
 const fs = require('fs');
 const path = require('path');
+const {performance} = require('perf_hooks');
 
 const dayToRun = parseInt(process.env.DAY) || 0;
 const sourceFolder = 'src';
 
-const filteredDayFiles = fs.readdirSync(sourceFolder)
+fs.readdirSync(sourceFolder)
     .filter(file => file.endsWith('.js'))
-    .filter(file => dayToRun > 0 ? file === `day-${dayToRun}.js` : true);
-
-if (filteredDayFiles.length === 0) {
-    console.log(`Nothing to run`);
-} else {
-    Promise.allSettled(filteredDayFiles.map(dayFile => {
-        const match = dayFile.match(/day-(.*).js/);
+    .filter(file => dayToRun > 0 ? file === `day-${dayToRun}.js` : true)
+    .map(file => path.join(__dirname, sourceFolder, file))
+    .map(file => {
+        const match = file.match(/day-(.*).js/);
         const dayNumber = match !== null ? match[1] : '???';
-        return import(`./${path.join(sourceFolder, dayFile)}`).then(dayExports => {
-            return {
-                day: dayNumber,
-                exports: dayExports
-            };
-        }).catch(error => {
-            throw {
-                day: dayNumber,
-                error: error
-            };
-        })
-    })).then(dayImports => {
-        dayImports.forEach((dayImport) => {
-            const log = [];
-            switch (dayImport.status) {
-                case 'fulfilled':
-                    log.push(`==== DAY #${dayImport.value.day}`, ...Object.entries(dayImport.value.exports.default).map(([exportKey, exportValue]) => `${exportKey} => ${exportValue}`));
-                    break
-                case 'rejected':
-                    log.push(`==== DAY #${dayImport.reason.day}`, `[x] ${dayImport.reason.error}`);
-                    break;
+        const start = performance.now();
+        let exports;
+        let error;
+        try {
+            if (global.gc) {
+                global.gc();
             }
-            console.log(log.join('\n'));
-        });
-    }).catch(ex => console.error(ex));
-}
+            exports = require(file);
+        } catch (ex) {
+            error = ex;
+        }
+
+        const stats = [
+            `ex. time ${(performance.now() - start).toFixed(2)} ms`
+        ];
+        if (global.gc) {
+            stats.push(`mem. used: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`);
+        }
+
+        return {
+            day: dayNumber,
+            exports,
+            error,
+            stats
+        }
+    }).forEach(o => {
+        const message = [`=== DAY #${o.day} (${o.stats.join(' | ')})`];
+        if (o.exports) {
+            message.push(...Object.entries(o.exports).map(([exportKey, exportValue]) => `|-> ${exportKey} => ${exportValue}`));
+        }
+        if (o.error) {
+            message.push(`[X] An error occurred while executing the script`, '');
+        }
+
+        console.log(message.join('\n'), o.error ? o.error : '');
+    });
