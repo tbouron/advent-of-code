a7efb1ebf2160b7fe88dc4de60c76fe3c247c6ce
Thomas Bouron <thomas.bouron@cloudsoftcorp.com>
2021-12-12 22:27:13 +0000

    Day 12: Add part #2

--- src/day-12.js
+++ src/day-12.js
@@ -6,18 +6,32 @@ const initialInput = fs.readFileSync(path.join(__dirname, `${path.basename(__fil
     .filter(l => l.trim() !== '')
     .map(l => l.split('-'));
 
-function numPaths(path, links) {
+function numPaths(path, links, maxNumberVisitsForSmallACave = 1) {
     const lastNode = path[path.length - 1];
+
     if (lastNode === 'end') {
         return 1;
     }
 
+    const hasReachedMaxNumberForASmallCave = Object.values(path.filter(n => n.match(/[a-z]+/)).reduce((map, node) => {
+        if (!map.hasOwnProperty(node)) {
+            map[node] = 0;
+        }
+        map[node]++;
+        return map;
+    }, {})).some(v => v >= maxNumberVisitsForSmallACave);
+
     const total = links
         .filter(link => link.includes(lastNode))
         .reduce((sum, link) => {
-            const nextNode = link.find(n => n !== lastNode && n !== 'start' && (n.match(/[A-Z]+/) || !path.includes(n)));
+            const nextNode = link.find(linkNode => linkNode !== lastNode
+                && linkNode !== 'start'
+                && (
+                    linkNode.match(/[A-Z]+/)
+                    || path.filter(pathNode => pathNode === linkNode).length < (hasReachedMaxNumberForASmallCave ? 1 : maxNumberVisitsForSmallACave)
+                ));
             if (nextNode) {
-                sum += numPaths(path.concat([nextNode]), links);
+                sum += numPaths(path.concat([nextNode]), links, maxNumberVisitsForSmallACave);
             }
             return sum;
         }, 0);
@@ -26,5 +40,6 @@ function numPaths(path, links) {
 }
 
 module.exports = {
-    'Part #1': numPaths(['start'], initialInput)
+    'Part #1': numPaths(['start'], initialInput),
+    'Part #2': numPaths(['start'], initialInput, 2)
 };
