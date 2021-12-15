6d2c63951500d61e1a7c23da42150d91d73e4c85
Thomas Bouron <thomas.bouron@cloudsoftcorp.com>
2021-12-15 16:54:19 +0000

    Day 15: Slightly more optimised version for part #1. Although it still doesn't work

--- src/day-15.js
+++ src/day-15.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-function dijkstra(grid, row = 0, col = 0, vertices = []) {
+function dijkstra(grid, row = 0, col = 0) {
     if (row >= grid.length) {
         throw new Error(`The row index [${row}] is out of bound`);
     }
@@ -10,87 +10,75 @@ function dijkstra(grid, row = 0, col = 0, vertices = []) {
     }
 
     const currentNode = grid[row][col];
+    currentNode.visited = true;
 
-    // console.log(`=> Go to [${row}] [${col}] | Risk ${currentNode.risk} | Distance ${currentNode.distance}`);
-
-    if (row === grid.length - 1 && col === grid[row].length - 1) {
-        return currentNode
-    }
+    const vertices = grid.flatMap(row => row.filter(col => col.visited === true));
 
     if (row === 0 && col === 0) {
-        vertices.push({
-            node: currentNode,
-            row,
-            col
-        });
+        vertices.push(currentNode);
+    }
+
+    // console.log(`=> Go to [${row}] [${col}] | Risk ${currentNode.risk} | Distance ${currentNode.distance} | Vertices`, vertices.length);
+
+    // This is for test on the actual input. I can manage to go up to the center point of the graph, but no further :(
+    // if (row === 45 && col === 45) {
+    if (row === grid.length - 1 && col === grid[row].length - 1) {
+        return currentNode
     }
 
     const neighbours = vertices.flatMap(vertex => {
         const ns = [];
         // UP
-        if (vertex.row > 0) {
-            ns.push({
-                node: grid[vertex.row - 1][vertex.col],
-                row: vertex.row - 1,
-                col: vertex.col
-            });
+        if (vertex.row > 0 && !vertices.includes(grid[vertex.row - 1][vertex.col])) {
+            ns.push(grid[vertex.row - 1][vertex.col]);
         }
         // LEFT
-        if (vertex.col > 0) {
-            ns.push({
-                node: grid[vertex.row][vertex.col - 1],
-                row: vertex.row,
-                col: vertex.col - 1
-            });
+        if (vertex.col > 0 && !vertices.includes(grid[vertex.row][vertex.col - 1])) {
+            ns.push(grid[vertex.row][vertex.col - 1]);
         }
         // RIGHT
-        if (vertex.col < grid[vertex.row].length - 1) {
-            ns.push({
-                node: grid[vertex.row][vertex.col + 1],
-                row: vertex.row,
-                col: vertex.col + 1
-            });
+        if (vertex.col < grid[vertex.row].length - 1 && !vertices.includes(grid[vertex.row][vertex.col + 1])) {
+            ns.push(grid[vertex.row][vertex.col + 1]);
         }
         // DOWN
-        if (vertex.row < grid.length - 1) {
-            ns.push({
-                node: grid[vertex.row + 1][vertex.col],
-                row: vertex.row + 1,
-                col: vertex.col
-            });
+        if (vertex.row < grid.length - 1 && !vertices.includes(grid[vertex.row + 1][vertex.col])) {
+            ns.push(grid[vertex.row + 1][vertex.col]);
         }
         return ns;
     });
 
     const sortedNeighbours = neighbours
-        .filter(ns => !vertices.some(v => ns.row === v.row && ns.col === v.col))
         .map(n => {
-            if (n.node.distance === 0 || n.node.distance > n.node.risk + currentNode.distance) {
-                n.node.distance = n.node.risk + currentNode.distance;
+            if (n.distance === 0 || n.distance > n.risk + currentNode.distance) {
+                n.distance = n.risk + currentNode.distance;
             }
             return n;
         })
-        .sort((a, b) => a.node.distance - b.node.distance);
+        .sort((a, b) => a.distance - b.distance);
 
     let nextNode;
     for (nextNode of sortedNeighbours) {
         if (!vertices.includes(nextNode)) {
-            vertices.push(nextNode);
             break;
         }
     }
 
-    return dijkstra(grid, nextNode.row, nextNode.col, vertices);
-}
+    if (!nextNode) {
+        throw new Error('Fail to get the next node');
+    }
 
+    return dijkstra(grid, nextNode.row, nextNode.col);
+}
 
 const initialInput = fs.readFileSync(path.join(__dirname, `${path.basename(__filename, '.js')}.txt`), 'utf8')
     .split('\n')
     .filter(l => l.trim() !== '')
-    .map(l => l.split('').map(item => {
+    .map((l, rowIndex) => l.split('').map((item, colIndex) => {
         return {
             risk: parseInt(item),
-            distance: 0
+            distance: 0,
+            row: rowIndex,
+            col: colIndex
         }
     }));
 
