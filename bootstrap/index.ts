8c95ccf5ece94a5736a61fb6dd2a4a8e5573ee3e
Thomas Bouron <thomas.bouron@cloudsoftcorp.com>
2022-12-06 23:26:24 +0100

    Improve data fetch to include the input data, and avoid refetching if the data is already saved locally

--- bootstrap/index.ts
+++ bootstrap/index.ts
@@ -2,6 +2,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 import axios from "axios";
 import {config} from 'dotenv';
+import * as jsdom from 'jsdom';
 
 config();
 
@@ -17,15 +18,42 @@ if (errors.length > 0) {
     throw new Error([...errors, 'You can specify the environment variable before the command, or in the .env file'].join('\n'))
 }
 
-axios.get<string>(
-    `https://adventofcode.com/${process.env.YEAR}/day/${process.env.DAY}/input`,
-    {
-        headers: {
-            Cookie: `session=${process.env.SESSION}`
+const baseUrl = `https://adventofcode.com/${process.env.YEAR}/day/${process.env.DAY}`;
+const inputFilePath = `../${process.env.YEAR}/src/day-${process.env.DAY}.txt`;
+const testInputFilePath = `../${process.env.YEAR}/src/day-${process.env.DAY}.test.txt`;
+
+if (!fs.existsSync(path.join(__dirname, `../${process.env.YEAR}/src/day-${process.env.DAY}.txt`))) {
+    axios.get<string>(
+        `${baseUrl}/input`,
+        {
+            headers: {
+                Cookie: `session=${process.env.SESSION}`
+            }
+        }).then(response => {
+        fs.writeFileSync(path.join(__dirname, inputFilePath), response.data);
+            console.log(`Input data written in ${inputFilePath}`);
+        }).catch(error => {
+            throw new Error(`Failed to get or write input data: ${error.message}`);
+        });
+} else {
+    console.log('Input data already fetched => Skipping')
+}
+
+if (!fs.existsSync(path.join(__dirname, `../${process.env.YEAR}/src/day-${process.env.DAY}.test.txt`))) {
+    axios.get<string>(baseUrl).then(response => {
+        let dom = new jsdom.JSDOM(response.data);
+        let htmlElement = dom.window.document.querySelector("code");
+        if (htmlElement === null) {
+            throw new Error('Input test data not found!');
         }
-    }).then(response => {
-        fs.writeFileSync(path.join(__dirname, `../${process.env.YEAR}/src/day-${process.env.DAY}.txt`), response.data);
-        console.log(`Input data written in ../${process.env.YEAR}/src/day-${process.env.DAY}.txt`);
+        fs.writeFileSync(path.join(__dirname, testInputFilePath), htmlElement.textContent ?? '');
+        console.log(`Test input data written in ${testInputFilePath}`);
     }).catch(error => {
-        throw new Error(`Failed to get or write input data: ${error.message}`);
+        throw new Error(`Failed to get or write test input data: ${error.message}`);
     });
+} else {
+    console.log('Test input data already fetched => Skipping');
+}
+
+
+
