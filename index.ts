f15df202af18203b260f66531e2b6f6324bf7abf
Thomas Bouron <thomas.bouron@cloudsoftcorp.com>
2024-12-07 11:17:11 +0100

    Add more useful display for the time spent in stats

--- index.ts
+++ index.ts
@@ -2,6 +2,54 @@ import * as fs from 'fs';
 import * as path from 'path';
 import {performance} from 'perf_hooks';
 
+const readableTime = (milliseconds: number) => {
+    const s = 1000;
+    const m = s * 60;
+    const h = m * 60;
+    const d = h * 24;
+    const w = d * 7;
+    const y = d * 365.25;
+    const time = {
+        year: 0,
+        week: 0,
+        day: 0,
+        hour: 0,
+        minute: 0,
+        second: 0,
+        millisecond: 0,
+    }
+    if (milliseconds > y) {
+        time.year = Math.floor(milliseconds / y);
+        milliseconds -= time.year * y;
+    }
+    if (milliseconds > w) {
+        time.week = Math.floor(milliseconds / w);
+        milliseconds -= time.week * w;
+    }
+    if (milliseconds > d) {
+        time.day = Math.floor(milliseconds / d);
+        milliseconds -= time.day * d;
+    }
+    if (milliseconds > h) {
+        time.hour = Math.floor(milliseconds / h);
+        milliseconds -= time.hour * h;
+    }
+    if (milliseconds > m) {
+        time.minute = Math.floor(milliseconds / m);
+        milliseconds -= time.minute * m;
+    }
+    if (milliseconds > s) {
+        time.second = Math.floor(milliseconds / s);
+        milliseconds -= time.second * s;
+    }
+    time.millisecond = milliseconds;
+
+    return Object.entries(time)
+        .filter(([unit, value]) => value > 0)
+        .map(([unit, value]) => `${value.toFixed(0)} ${unit}${value > 1 ? 's' : ''}`)
+        .join(', ');
+}
+
 const yearToRun = process.env.YEAR || new Date().getFullYear().toString();
 const dayToRun = parseInt(process.env.DAY || '0') || 0;
 
@@ -25,7 +73,7 @@ fs.readdirSync(yearToRun)
         }
 
         const stats = [
-            `ex. time ${(performance.now() - start).toFixed(2)} ms`,
+            `ex. time ${readableTime(performance.now() - start)}`,
             `mem. used: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`
         ];
 
@@ -51,6 +99,6 @@ fs.readdirSync(yearToRun)
 if (dayToRun === 0) {
     console.log([
         '\n',
-        `=== Total suite ex. time: ${(performance.now() - suiteStart).toFixed(2)} ms === `
+        `=== Total suite ex. time: ${readableTime(performance.now() - suiteStart)}  === `
     ].join('\n'));
 }
